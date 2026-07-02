08e5e655a0f2662b4acf9509c3a231bca8bb9ba5
Lord <ifatich@gmail.com>
2026-07-02 16:49:15 +0700

    fix(router): use createWebHashHistory for GitHub Pages compatibility

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 
 import MainLayout from '@/components/layouts/MainLayout.vue'
 import LoginView from '@/views/auth/LoginView.vue'
@@ -11,7 +11,7 @@ import TokenSwapView from '@/views/user/token/TokenSwapView.vue'
 import TokenTransferView from '@/views/user/token/TokenTransferView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
